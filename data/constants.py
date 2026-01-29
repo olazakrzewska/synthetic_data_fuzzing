c2624dc759a271ff47d8c36fbdd7a5b53693a14f
olazakrzewska <75899961+olazakrzewska@users.noreply.github.com>
2026-01-29 10:02:53 +0100

    Add files via upload

---
+++ data/constants.py
@@ -0,0 +1,17 @@
+columns = [
+    "age", "workclass", "fnlwgt", "education", "education-num",
+    "marital-status", "occupation", "relationship", "race", "sex",
+    "capital-gain", "capital-loss", "hours-per-week",
+    "native-country", "income"
+]
+
+numerical = [
+    "age", "fnlwgt", "education-num",
+    "capital-gain", "capital-loss", "hours-per-week"
+]
+
+categorical = [
+    "workclass", "education", "marital-status",
+    "occupation", "relationship", "race", "sex",
+    "native-country", "income"
+]
